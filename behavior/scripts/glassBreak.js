92d7a7af48f57f01eddac45d3cee003501a5ceb3
altivelis <ayupon89821026@gmail.com>
2023-04-25 00:01:20 +0900

    コマンドバグ修正

--- behavior/scripts/glassBreak.js
+++ behavior/scripts/glassBreak.js
@@ -25,8 +25,8 @@ export async function breakGlass(origin){
     let WhiteList = [[origin.x,origin.y,origin.z]];
     let originGlass = getGlass(origin);
     console.log(originGlass.color);
-    runWorld(origin.dimension.id,`setblock ${origin.x} ${origin.y} ${origin.z} air 0 destroy`);
-    runWorld(origin.dimension.id,`summon altivelis:marker ${origin.x} ${origin.y} ${origin.z} glass "${originGlass.blockId}${(originGlass.color)?`.${originGlass.color}`:""}"`);
+    runWorld(origin.dimension.id,`setblock ${origin.x} ${origin.y} ${origin.z} air [] destroy`);
+    runWorld(origin.dimension.id,`summon altivelis:marker ${origin.x} ${origin.y} ${origin.z} ~ ~ glass "${originGlass.blockId}${(originGlass.color)?`.${originGlass.color}`:""}"`);
     mc.system.run(function code(){
         for(let i=0;i<20;i++){
             if(WhiteList.length==0)return;
@@ -39,8 +39,8 @@ export async function breakGlass(origin){
                 let block = origin.dimension.getBlock({x:pos[0]+x,y:pos[1]+y,z:pos[2]+z});
                 let glass = getGlass(block);
                 if(glass){
-                    runWorld(block.dimension.id,`setblock ${block.x} ${block.y} ${block.z} air 0 destroy`);
-                    runWorld(block.dimension.id,`summon altivelis:marker ${block.x} ${block.y} ${block.z} glass "${glass.blockId}${(glass.color)?`.${glass.color}`:""}"`);
+                    runWorld(block.dimension.id,`setblock ${block.x} ${block.y} ${block.z} air [] destroy`);
+                    runWorld(block.dimension.id,`summon altivelis:marker ${block.x} ${block.y} ${block.z} ~ ~ glass "${glass.blockId}${(glass.color)?`.${glass.color}`:""}"`);
                     WhiteList.push([block.x,block.y,block.z]);
                 }else{
                     BlockList.push([block.x,block.y,block.z]);
